b00882a2d68f453f5b7548c1a43814f00445e2ce
lacsar712 <lacsar712@users.noreply.github.com>
2026-08-24 21:14:04 +0800

    init: asphaltm

---
+++ internal/clock/window.go
@@ -0,0 +1,30 @@
+package clock
+
+import (
+	"time"
+
+	"github.com/lacsar712/asphaltm/internal/model"
+)
+
+type HeatWindow struct {
+	clk      Clock
+	duration time.Duration
+}
+
+func NewHeatWindow(clk Clock, duration time.Duration) *HeatWindow {
+	if duration <= 0 {
+		duration = 2 * time.Minute
+	}
+	return &HeatWindow{clk: clk, duration: duration}
+}
+
+func (w *HeatWindow) Active(anchor time.Time) bool {
+	return time.Since(anchor) < w.duration
+}
+
+func (w *HeatWindow) Require(anchor time.Time) error {
+	if w.Active(anchor) {
+		return nil
+	}
+	return model.ErrHeatHold
+}
